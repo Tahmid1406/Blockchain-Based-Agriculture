be59499bf840e5209eaa68579a0637350709fb46
tahmid1406 <tahmidpranto1406@gmail.com>
2020-09-11 02:18:27 +0600

    storage contract improved

--- Storage.sol
+++ Storage.sol
@@ -60,7 +60,7 @@ contract Storage{
         uint price,
         uint optTem,
         uint optHum,
-        uint optLitEx) private OnlyStorage{
+        uint optLitEx) private OnlyOwner{
             
         seedName = name;
         batch_id = bat_id;
@@ -72,58 +72,76 @@ contract Storage{
         
     }
     
-    function TemperatureSelfCheck(uint value) public view OnlyStorage returns(string memory){
+    function temperatureSelfCheck(uint value) private OnlyStorage returns(string memory){
         if(value > optimumTemp){
+            violationTrigger(ViolationType.Temperature, 1);
             return "Current temperature is over the threshold";
         }else if(value < optimumTemp){
+            violationTrigger(ViolationType.Temperature, 0);
             return "Current temperature is below the threshold";
         }else{
+            violationTrigger(ViolationType.Temperature, 2);
             return "Current temperature is optimum";
         }
     }
     
-    function HummiditySelfCheck(uint value) public view OnlyStorage returns(string memory){
+    function hummiditySelfCheck(uint value) private OnlyStorage returns(string memory){
         if(value > optimumHum){
+            violationTrigger(ViolationType.Hummidity, 1);
             return "Current hummidity is over the threshold";
         }else if(value < optimumTemp){
+            violationTrigger(ViolationType.Hummidity, 0);
             return "Current hummidity is below the threshold";
         }else{
+            violationTrigger(ViolationType.Hummidity, 2);
             return "Current hummidity is optimum";
         }
     }
     
-    function LightExpoSelfCheck(uint value) public view OnlyStorage returns(string memory){
+    function lightExpoSelfCheck(uint value) private OnlyStorage returns(string memory){
         if(value > optimumLightExpo){
+            violationTrigger(ViolationType.LightExposure, 1);
             return "Current light exposure is over the threshold";
         }else if(value < optimumTemp){
+            violationTrigger(ViolationType.LightExposure, 0);
             return "Current light exposure is below the threshold";
         }else{
+            violationTrigger(ViolationType.None, 2);
             return "Current light exposure is optimum";
         }
     }
     
-    function ViolationTrigger(ViolationType vio, int category) 
-        internal OnlyStorage{ //category 1 = over, 0 = under
+    function violationTrigger(ViolationType vio, int category) 
+        private OnlyStorage{ //category 0 = under, 1 = over, 2 optimum 
         
         if(vio == ViolationType.Temperature){
             if(category == 1){
                 tempCOn = TempCondition.Over;
                 violationType = ViolationType.Temperature;
                 emit TemperatureViolation(storage_address, "Temparature is over the threshold");  
-            }else{
+            }else if(category == 0){
                 tempCOn = TempCondition.Under;
                 violationType = ViolationType.Temperature;
                 emit TemperatureViolation(storage_address, "Temparature is below the threshold");
+            }else if(category == 2){
+                tempCOn = TempCondition.Optimum;
+                violationType = ViolationType.None;
+                emit TemperatureViolation(storage_address, "Temparature is Optimum");
+                
             }
         }else if(vio == ViolationType.Hummidity){
             if(category == 1){
                 humCOn = HumCondition.Over;
                 violationType = ViolationType.Hummidity;
                 emit HummidityViolation(storage_address, "Hummidity is over the threshold"); 
-            }else{
+            }else if(category == 0){
                 humCOn = HumCondition.Under;
                 violationType = ViolationType.Hummidity;
                 emit HummidityViolation(storage_address, "Hummidity is below the threshold");
+            }else if(category == 2){
+                humCOn = HumCondition.Optimum;
+                violationType = ViolationType.None;
+                emit HummidityViolation(storage_address, "Hummidity is Optimum");
             }
             
         }else if(vio == ViolationType.LightExposure){
@@ -131,13 +149,17 @@ contract Storage{
                 lightCOn = LightExpoCondition.Over;
                 violationType = ViolationType.LightExposure;
                 emit LightExposureViolation(storage_address, "Light exposure is over the threshold");
-            }else{
+            }else if(category == 0){
                 lightCOn = LightExpoCondition.Under;
                 violationType = ViolationType.LightExposure;
                 emit LightExposureViolation(storage_address, "Light exposure is below the threshold");
+            }else if(category == 2){
+                lightCOn = LightExpoCondition.Optimum;
+                violationType = ViolationType.None;
+                emit LightExposureViolation(storage_address, "Light exposure is optimum");
             }
         }
     }
-     
+    
     
 }
